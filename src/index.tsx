99f3ba584b00c18cdc068bf49aa6009d179f9a72
alexander <alexanderpershinwebdeveloper@gmail.com>
2022-10-09 18:16:46 +0700

    sass and bootstrap added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./global_styles/style.sass";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
