0aa42ba164f12a9de62b62b7de34e1b18db9e7be
alexander <alexanderpershinwebdeveloper@gmail.com>
2022-10-09 22:04:02 +0700

    changes made for npm publish

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./global_styles/style.sass";
+import SignForm from "./lib/SignForm";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -17,3 +18,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export { SignForm };
--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import SignForm from "./SignForm";
-
-const returnLibrary = () => {
-    return {
-        SignForm,
-    };
-};
-export default returnLibrary();
